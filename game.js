24afd4be29c134f1f33b5f3a186c3e6e70c85e43
Shauna Dunn <shaunadunn1@gmail.com>
2020-12-12 10:18:06 -0500

    added 2 working functions

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var selectedAnswerCapLetter = Array.from(document.querySelectorAll("#choice-option"));
 var selectedAnswerText = Array.from(document.querySelectorAll(".choice-text"));
 var scoreText = document.querySelector("#display-score");
-var questionDisplay = document.querySelector("#display-question");
+const question = document.getElementById("display-question");
 var startButton = document.querySelector("#start-button")
 
 var currentQuestion = {};
@@ -10,8 +10,7 @@ var acceptAnswer = true;
 var questionCounter = 0;
 var availableQuestions = [];
 
-startButton.addEventListener("click," startTimer);
-
+startButton.addEventListener("click", startTimer);
 
 var questions = [
     {
@@ -19,7 +18,7 @@ var questions = [
         choice1: "yarn",
         choice2: "a number",
         choice3: "cheese",
-        choice4: "a line of text"
+        choice4: "a line of text",
         answer: 4,
     },
 
@@ -28,7 +27,7 @@ var questions = [
         choice1: "Dance On Mars",
         choice2: "Document Object Model",
         choice3: "Disc Open Module",
-        choice4: "Destroy Oversize Motorboats"
+        choice4: "Destroy Oversize Motorboats",
         answer: 2,
     },
 
@@ -37,7 +36,7 @@ var questions = [
         choice1: "a way to generate fancy text",
         choice2: "a function to calculate random numbers",
         choice3: "a list of items stored within a variable",
-        choice4: "stops a countdown at zero"
+        choice4: "stops a countdown at zero",
         answer: 3,
     },
 
@@ -46,10 +45,34 @@ var questions = [
         choice1: "magic",
         choice2: "through the DOM",
         choice3: "sends a text",
-        choice4: "without even trying"
+        choice4: "without even trying",
         answer: 2,
     },
 ]
+
+const CORRECT_SCORE = 10;
+const MAX_QUESTIONS = 4;
+
+function startGame() {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewQuestion();
+};
+
+function getNewQuestion() {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+};
+
+startGame();
+getNewQuestion();
+
+
+
 
 // function startTimer() {
 //     var seconds = document.getElementById("countdown").textContent;
@@ -60,7 +83,7 @@ var questions = [
 //     }, 1000);
 // }
 
-function startTimer(){
+function startTimer() {
     var counter = 60;
     setInterval(function() {
       counter--;
