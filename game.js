ebf15e82563295add975e5ed9e653fc95a735ebb
Shauna Dunn <shaunadunn1@gmail.com>
2020-12-13 23:55:23 -0500

    random sequence of questions/choices appear

--- game.js
+++ game.js
@@ -1,17 +1,20 @@
-var selectedAnswerCapLetter = Array.from(document.querySelectorAll("#choice-option"));
-var selectedAnswerText = Array.from(document.querySelectorAll(".choice-text"));
+// Variables declared in reference to ids and classes in html document
+// var selectedAnswerCapLetter = Array.from(document.querySelectorAll("#choice-option"));
+const choices = Array.from(document.querySelectorAll(".choice-text"));
 var scoreText = document.querySelector("#display-score");
 const question = document.getElementById("display-question");
 var startButton = document.querySelector("#start-button")
 
+// Local variables declared to be used in functions below
 var currentQuestion = {};
 var score = 0;
-var acceptAnswer = true;
+var acceptAnswer = false;
 var questionCounter = 0;
 var availableQuestions = [];
 
 startButton.addEventListener("click", startTimer);
 
+// Questions object created for quiz
 var questions = [
     {
         question: "What is a string?",
@@ -50,38 +53,59 @@ var questions = [
     },
 ]
 
+// constants declared and assigned for parameters of quiz
 const CORRECT_SCORE = 10;
 const MAX_QUESTIONS = 4;
 
+// startGame sets score to zero and starts with question 1 out of 4 
 function startGame() {
     questionCounter = 0;
     score = 0;
+    // spread syntax used below to list questions within the variable availableQuestions
     availableQuestions = [...questions];
     console.log(availableQuestions);
+    // calls a new function getNewQuestion to run
     getNewQuestion();
 };
 
-function getNewQuestion() {
+// getNewQuestion increments the questionCounter as the user moves through the quiz
+    getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem("mostRecentScore", score)
+    }
+    
     questionCounter++;
+    /* a new constant is declared to randomly select questions from our object questions and display them via the 
+     variable currentQuestion */
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptAnswer = true;
 };
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if(!acceptAnswer) return
+
+        acceptAnswer = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+
+        console.log(selectedAnswer, currentQuestion.answer)
+        getNewQuestion();
+    });
+});
 
 startGame();
 getNewQuestion();
-
-
-
-
-// function startTimer() {
-//     var seconds = document.getElementById("countdown").textContent;
-//     var countdown = setInterval(function() {
-//         seconds--;
-//         document.getElementById("countdown").textContent = seconds;
-//         if (seconds <= 0) clearInterval(countdown);
-//     }, 1000);
-// }
 
 function startTimer() {
     var counter = 60;
